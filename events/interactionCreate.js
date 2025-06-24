350229b91c3d91684eeeb28347187644a285c33f
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-06-24 23:25:33 +0900

    各処理をファイルに分離

---
+++ events/interactionCreate.js
@@ -0,0 +1,34 @@
+const { MessageFlags } = require("discord.js");
+
+module.exports = async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const appMode = process.env.APP_MODE;
+  if (appMode !== "production") {
+    console.log(interaction);
+  }
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
+};
